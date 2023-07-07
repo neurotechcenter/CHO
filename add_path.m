ee2d0f9f2b2e44e5340bb351f0210fdef45a37c9
Hohyun Cho <73135444+hohyuncho@users.noreply.github.com>
2023-07-07 16:37:51 -0500

    Add files via upload

---
+++ add_path.m
@@ -0,0 +1,13 @@
+
+addpath(genpath('./functions'));
+addpath(genpath('./data'));
+
+addpath('/Volumes/T7/Dropbox/Dropbox/matlab/fieldtrip-20230118');
+
+ft_defaults
+
+run('/Volumes/T7/Dropbox/Dropbox/matlab/eeglab2021.0/eeglab');
+
+clear
+clc
+close all
---
+++ figure8BCDE.m
@@ -0,0 +1,16 @@
+clear;
+clc;
+
+
+load data_for_figure8.mat
+srate = 500;
+
+
+%%
+param.plot = 1;
+param.minimum_cycles = 2;
+param.ovlp_threshold = 0.50;
+param.frequency_vector = 1:40;
+
+[cho_output] = CHO_v21(raw_data, srate, param);
+
