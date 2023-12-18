89c604db39a3d2e0f32105e06ffa6e9f0c39a9bf
Hohyun Cho <73135444+hohyuncho@users.noreply.github.com>
2023-12-18 16:54:18 -0600

    Add files via upload

--- figure8BCDE.m
+++ figure8BCDE.m
@@ -12,5 +12,6 @@ param.minimum_cycles = 2;
 param.ovlp_threshold = 0.50;
 param.frequency_vector = 1:40;
 
-[cho_output] = CHO_v21(raw_data, srate, param);
+[cho_output] = CHO_v22(raw_data, srate, param);
+
 
